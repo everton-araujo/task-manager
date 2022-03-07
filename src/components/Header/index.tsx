fe52f4d82782215a5f48fee272fd60dee917bb0a
Everton <everton.aaraujo@outlook.com>
2022-03-06 21:04:06 -0300

    About page

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,40 @@
+import Link from 'next/link'
+import { useState } from 'react'
+
 import { SignInButton } from '../SignInButton'
 
 import styles from './styles.module.scss'
 
 export function Header() {
+  const [isHomeActive, setIsHomeActive] = useState(true);
+  const [isAboutActive, setIsAboutActive] = useState(false);
+
+  function handleActiveTab(tab: string) {
+    if (tab === 'home') {
+      setIsHomeActive(true)
+      setIsAboutActive(false)
+    } else {
+      setIsAboutActive(true)
+      setIsHomeActive(false)
+    }
+  }
+
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         <img src="/images/logo.png" alt="task manager logo" />
 
         <nav>
-          <a className={styles.active}>Home</a>
-          <a>Sobre</a>
+          <Link href='/'>
+            <a className={isHomeActive ? styles.active : ''} onClick={() => handleActiveTab('home')}>
+              Home
+            </a>
+          </Link>
+          <Link href='/about'>
+            <a className={isAboutActive ? styles.active : ''} onClick={() => handleActiveTab('about')}>
+              Sobre
+            </a>
+          </Link>
         </nav>
 
         <SignInButton />
---
+++ src/pages/about/index.tsx
@@ -0,0 +1,27 @@
+import styles from './styles.module.scss'
+
+export default function About() {
+  return (
+    <main className={styles.content}>
+      <h1>Sobre nós</h1>
+      <h3>Porque nascemos</h3>
+      <p>
+        Nascemos com o intuito em ajudar na organização do gerenciamento de projetos
+
+      </p>
+
+      <h2>Questões técnicas</h2>
+      <h3>Porque só temos login social</h3>
+      <p>
+        A principio a página foi construída sem back-end, portanto a validação se faz por OAuth e por enquanto a conta escolhida foi github. <br />
+        Posteriormente ganhará autenticação com Google e ao longo que o projeto crescer, terá uma autenticação própria quando um servidor back-end for construído.
+      </p>
+
+      <h3>Gostaria de contribuir?</h3>
+      <p>
+        O projeto é open source e o repositório do github se encontra
+        <a href="https://github.com/everton-araujo/task-manager" target="_blank"> aqui</a>.
+      </p>
+    </main>
+  )
+}
